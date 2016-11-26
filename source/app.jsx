6584f249d818e6ae494edb2d7a951b75d887d0ac
yeffasol <yeffasol@mail.ru>
2016-11-26 19:08:40 +0300

    intro ready apps for programmers

--- source/app.jsx
+++ source/app.jsx
@@ -1,7 +1,69 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-var text =
-  <h1>hello</h1>;
+var ImageCounter = function (props) {
+  return (
+    <div className="image-counter">
+      <div className="count">{props.count} </div>
+      <img src={'img/' + props.url} width="100" onClick={props.onCount}/>
+    </div>
+  )
+};
+var Hero = React.createClass({
+  getInitialState: function () {
+    return {
+      count: 0
+    };
+  },
 
-ReactDOM.render(text, document.getElementById('root'));
+  handleClick: function () {
+    this.setState({count: this.state.count + 1});
+  },
+
+  render: function () {
+    return (
+      <div className="container">
+        <ImageCounter url={this.props.url} count={this.state.count} onCount={this.handleClick}/>
+        <h1>{this.props.title}</h1>
+        <p>{this.props.subtitle}</p>
+      </div>
+    )
+  }
+});
+
+var App = React.createClass({
+  render: function () {
+    return (
+      <div>
+        {this.props.heroes.map(function (hero, i) {
+          return <Hero key={i} title={hero.title}
+                       subtitle={hero.subtitle}
+                       url={hero.url}/>
+        })}
+      </div>
+    )
+  }
+});
+var data = [
+  {
+    title: "React",
+    subtitle: "Библиотека для создания пользовательских приложений",
+    url: "react.png"
+  },
+  {
+    title: "Angular",
+    subtitle: "Один фреймворк",
+    url: "angular.png"
+  },
+  {
+    title: "Ember",
+    subtitle: "фреймворк для создания амбициозных веб-приложений",
+    url: "ember.png"
+  },
+  {
+    title: "Vue",
+    subtitle: "Прогрессивный фреймворк",
+    url: "vue.png"
+  }
+];
+ReactDOM.render(<App heroes={data}/>, document.getElementById('root'));
