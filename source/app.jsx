7ac194b95b8ed9b8efbb5f2a2e812867d59f0dac
yeffasol <yeffasol@mail.ru>
2016-11-26 17:36:12 +0300

    first coomit hello

---
+++ source/app.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+var text =
+  <h1>hello</h1>;
+
+ReactDOM.render(text, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './source/App.jsx',
+
+  output: {
+    filename: 'bundle.js',
+    path: './public'
+  },
+
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      loader:'babel',
+      query: {
+        presets: ['es2015','react']
+      }
+    }]
+  },
+
+  devtool: '#cheap-module-source-map'
+};
